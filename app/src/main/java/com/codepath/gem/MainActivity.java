0deddf5bac7b2c4d9008f2ca9af7c91a55eaab1e
dhrutip <dhruti.pandya@gmail.com>
2021-07-13 10:27:55 -0700

    completed fragment manager for bottom navigation view

--- app/src/main/java/com/codepath/gem/MainActivity.java
+++ app/src/main/java/com/codepath/gem/MainActivity.java
@@ -1,14 +1,56 @@
 package com.codepath.gem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.codepath.gem.fragments.CommitmentsFragment;
+import com.codepath.gem.fragments.CreateFragment;
+import com.codepath.gem.fragments.HomeFragment;
+import com.codepath.gem.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_create:
+                        fragment = new CreateFragment();
+                        break;
+                    case R.id.action_commitments:
+                        fragment = new CommitmentsFragment();
+                        break;
+                    case R.id.action_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                    default:
+                        return true;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_home); // default selection
     }
 }
