2f0d1ac0a33715622e6c5c20a5c76ae6f2e78574
dhrutip <dhruti.pandya@gmail.com>
2021-07-14 09:22:04 -0700

    user can log out

--- app/src/main/java/com/codepath/gem/MainActivity.java
+++ app/src/main/java/com/codepath/gem/MainActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.codepath.gem.fragments.CommitmentsFragment;
 import com.codepath.gem.fragments.CreateFragment;
 import com.codepath.gem.fragments.HomeFragment;
 import com.codepath.gem.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -53,5 +59,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         bottomNavigationView.setSelectedItemId(R.id.action_home); // default selection
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.btnLogout) {
+            onLogoutButton();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onLogoutButton() {
+        ParseUser.logOutInBackground(new LogOutCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    goLoginActivity();
+                } else {
+                    Toast.makeText(MainActivity.this, "logout issue:(", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+    }
+
+    private void goLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
