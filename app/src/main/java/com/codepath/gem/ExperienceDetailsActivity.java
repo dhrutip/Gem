029ef0d5fd093ac59b15fbcb119ec0ed4f623c0b
dhrutip <dhruti.pandya@gmail.com>
2021-07-15 11:04:06 -0700

    added visual indicator (colored heart) that persists for favorited experiences

--- app/src/main/java/com/codepath/gem/ExperienceDetailsActivity.java
+++ app/src/main/java/com/codepath/gem/ExperienceDetailsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -38,6 +39,8 @@ public class ExperienceDetailsActivity extends AppCompatActivity {
     ImageView ivDetailsImageOne;
     ImageView ivDetailsImageTwo;
     ImageButton btnDetailsAddInterest;
+    boolean commitmentExists;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +52,7 @@ public class ExperienceDetailsActivity extends AppCompatActivity {
         ivDetailsImageOne = findViewById(R.id.ivDetailsImageOne);
         ivDetailsImageTwo = findViewById(R.id.ivDetailsImageTwo);
         btnDetailsAddInterest = findViewById(R.id.btnDetailsAddInterest);
+        context = this;
 
         experience = (Experience) Parcels.unwrap(getIntent().getParcelableExtra(Experience.class.getSimpleName()));
         tvDetailsTitle.setText(experience.getTitle());
@@ -63,18 +67,29 @@ public class ExperienceDetailsActivity extends AppCompatActivity {
                     .load(experience.getImageTwo().getUrl())
                     .into(ivDetailsImageTwo);
         }
-
-        btnDetailsAddInterest.setOnTouchListener(new OnDoubleTapListener(this) {
+        // fill heart icon if already favorited, otherwise allow user to add a commitment
+        checkCommitment();
+        Handler handler = new Handler(); // required for checkCommitment() to finish first
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onDoubleTap(MotionEvent e) {
-                createCommitment();
-                btnDetailsAddInterest.setImageResource(R.drawable.favorite_filled);
+            public void run() {
+                if (commitmentExists == true) {
+                    btnDetailsAddInterest.setImageResource(R.drawable.favorite_filled);
+                } else {
+                    btnDetailsAddInterest.setOnTouchListener(new OnDoubleTapListener(context) {
+                        @Override
+                        public void onDoubleTap(MotionEvent e) {
+                            createCommitment();
+                            btnDetailsAddInterest.setImageResource(R.drawable.favorite_filled);
+                        }
+                    });
+                }
             }
-        });
+        }, 400);
     }
 
-    private void createCommitment() {
-        Log.i(TAG, "attempting to create commitment");
+    private void checkCommitment() {
+        // Toast.makeText(ExperienceDetailsActivity.this, "checking for commitment", Toast.LENGTH_SHORT).show();
         // check if commitment already exists
         ParseQuery<Commitment> query = ParseQuery.getQuery(Commitment.class);
         query.whereEqualTo(Commitment.KEY_USER, ParseUser.getCurrentUser());
@@ -85,16 +100,22 @@ public class ExperienceDetailsActivity extends AppCompatActivity {
                     final int statusCode = e.getCode();
                     if (statusCode == ParseException.OBJECT_NOT_FOUND) {
                         // commitment did not exist on the parse backend
-                        ParseObject newCommitment = ParseObject.create("Commitment");
-                        newCommitment.put(Commitment.KEY_USER, ParseUser.getCurrentUser());
-                        newCommitment.put(Commitment.KEY_EXPERIENCE, experience);
-                        newCommitment.saveInBackground();
+                        commitmentExists = false;
                     }
                 } else {
                     Log.i(TAG, "commitment already exists, no need for new commitment");
-                    Toast.makeText(ExperienceDetailsActivity.this, "commitment already exists!", Toast.LENGTH_SHORT).show();
+                    // Toast.makeText(ExperienceDetailsActivity.this, "commitment already exists!", Toast.LENGTH_SHORT).show();
+                    commitmentExists = true;
                 }
             }
         });
+    }
+
+    public void createCommitment() {
+        // Toast.makeText(ExperienceDetailsActivity.this, "creating new commitment!", Toast.LENGTH_SHORT).show();
+        ParseObject newCommitment = ParseObject.create("Commitment");
+        newCommitment.put(Commitment.KEY_USER, ParseUser.getCurrentUser());
+        newCommitment.put(Commitment.KEY_EXPERIENCE, experience);
+        newCommitment.saveInBackground();
     }
 }
