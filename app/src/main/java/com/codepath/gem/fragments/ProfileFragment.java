abe0b71540d8c8033bff33eb0ef0f959dc10000e
dhrutip <dhruti.pandya@gmail.com>
2021-07-16 15:39:44 -0700

    user can view profile pic and username on profile fragment

--- app/src/main/java/com/codepath/gem/fragments/ProfileFragment.java
+++ app/src/main/java/com/codepath/gem/fragments/ProfileFragment.java
@@ -2,59 +2,34 @@ package com.codepath.gem.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.gem.R;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView tvUsername;
+    ImageView ivProfilePic;
 
     public ProfileFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +37,22 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvUsername = view.findViewById(R.id.tvUsername);
+        ivProfilePic = view.findViewById(R.id.ivProfilePic);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        tvUsername.setText(currentUser.getUsername());
+        ParseFile profilePic = currentUser.getParseFile("profilePic");
+        if (profilePic != null) {
+            Glide.with(view.getContext())
+                    .load(profilePic.getUrl())
+                    .circleCrop()
+                    .into(ivProfilePic);
+        }
     }
 }
